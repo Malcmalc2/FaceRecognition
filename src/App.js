1734d4336ead942a053b7ff04f3e909ef0177fb0
Malcolm Curtis <itcurtis@hotmail.com>
2019-10-04 22:33:03 -0400

    Heroku Change

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('https://git.heroku.com/agile-beach-03181.git/imageurl', {
+    fetch('https://agile-beach-03181.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -87,7 +87,7 @@ class App extends Component {
     .then(response => response.json())
       .then(response => {
         if(response) {
-          fetch('https://git.heroku.com/agile-beach-03181.git/image', {
+          fetch('https://agile-beach-03181.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://git.heroku.com/agile-beach-03181.git/signin', {
+    fetch('https://agile-beach-03181.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
